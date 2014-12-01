bbf4dd0f2a1f48105e7b9daacb36553529a2b7c4
amm28964 <amm28964@AN093629-CT122.nvstu.edu>
2014-12-01 17:54:49 -0500

    Added Article class and described a few fields.

---
+++ WikiSpider/src/java/wiki/Article.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2014 Xan Mead
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package wiki;
+
+import java.util.LinkedList; // Replace with own ADT
+import org.w3c.dom.Element;
+
+/**
+ * 
+ * @author Xan Mead
+ */
+public class Article {
+	
+	/** Name of the article. */
+	private String name;
+	
+	/** Root node of the article's main content. */
+	private Element body;
+	
+	/** Nodes that constitute the body of the Wikipedia article. */
+	private LinkedList<String> content;
+	
+	/** Categories in which this article is included. */
+	private LinkedList<String> categories;
+	
+	/** Other articles referenced within the article. */
+	private LinkedList<String> links;
+}
